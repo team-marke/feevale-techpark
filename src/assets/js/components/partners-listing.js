15af9578f33f8eaa9024858f4e292e4258bcb099
marke-franco-nienow <franco@marke.com.br>
2021-11-03 14:24:24 -0300

    Implementação do filtro JS de parceiros finalizado

--- src/assets/js/components/partners-listing.js
+++ src/assets/js/components/partners-listing.js
@@ -1,8 +1,27 @@
 class PartnersListing {
-  constructor (el) {
+  constructor(el) {
     this.el = el;
     this.search = el.querySelector('.partners-listing__search');
-    this.results = el.querySelector('.partners-listing__results');
+    this.partnersList = el.querySelector('.partners-listing__partners').children;
+    this._addLiteners();
+  }
+
+  _addLiteners() {
+    this.search.addEventListener('input', () => {
+      this.updatePartnersList();
+    });
+  }
+
+  updatePartnersList() {
+    const searchedValue = this.search.value.toLowerCase();
+    for (const partner of this.partnersList) {
+      const partnerName = partner.dataset.partnerName.toLowerCase();
+      if (partnerName.indexOf(searchedValue) >= 0) {
+        partner.classList.remove('partner-hidden');
+      } else {
+        partner.classList.add('partner-hidden');
+      }
+    }
   }
 }
 
