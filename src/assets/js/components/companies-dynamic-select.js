72dbd53ad7a17ea83923962e52a0830e8d0002c9
marke-franco-nienow <franco@marke.com.br>
2021-11-10 14:32:34 -0300

    Filtro dinâmico de empresas por unidades finalizado

--- src/assets/js/components/companies-dynamic-select.js
+++ src/assets/js/components/companies-dynamic-select.js
@@ -2,22 +2,64 @@ class CompaniesDynamicSelect {
   constructor(el, companiesContainer) {
     this.el = el;
     this.companiesContainer = companiesContainer;
+    this.mainPathName = '/nossas-empresas/';
     this._addListeners();
   }
 
   _addListeners() {
-    this.el.addEventListener('change', () => {
-      this.filterCompaniesByUnitSlug(this.el.value);
-
-      // Troca a URL com history API para o slug com página correspondente
-      this.updateURL(this.el.value)
+    window.addEventListener('load', () => {
+      this.handlePageLoad();
     });
+
+    window.addEventListener('popstate', () => {
+      this.handlePopstate();
+    });
+
+    this.el.addEventListener('change', () => {
+      this.handleChange();
+    });
+  }
+
+  handlePageLoad() {
+    const selectedValue = window.localStorage.getItem('selected-company');
+    if (selectedValue && this.isMainPage()) {
+      this.filterCompaniesByUnitSlug(selectedValue);
+      this.updateSelectValue(selectedValue);
+      this.updateURL(selectedValue);
+      window.localStorage.removeItem('selected-company');
+    }
+  }
+
+  handleChange() {
+    this.ifSubpageRedirectToMain(this.el.value);
+    this.filterCompaniesByUnitSlug(this.el.value);
+    this.updateURL(this.el.value);
+  }
+
+  handlePopstate() {
+    this.filterCompaniesByUnitSlug(history.state);
+    this.updateSelectValue(history.state);
+  }
+
+  isMainPage() {
+    const canonical = document.querySelector("link[rel='canonical']");
+    const url = new URL(canonical.href);
+    return url.pathname === this.mainPathName;
+  }
+
+  ifSubpageRedirectToMain(unitSlug) {
+    if (!this.isMainPage()) {
+      window.localStorage.setItem('selected-company', unitSlug);
+      window.location.replace(this.mainPathName);
+    } else {
+      window.localStorage.removeItem('selected-company');
+    }
   }
 
   filterCompaniesByUnitSlug(unitSlug) {
     const companies = Array.from(this.companiesContainer.children);
     companies.forEach((company) => {
-      if (company.dataset.unitSlug === unitSlug || !unitSlug) {
+      if (company.dataset.unitSlug === unitSlug || unitSlug === 'all') {
         company.classList.remove('d-none');
       } else {
         company.classList.add('d-none');
@@ -25,8 +67,13 @@ class CompaniesDynamicSelect {
     });
   }
 
+  updateSelectValue(unitSlug) {
+    this.el.value = unitSlug;
+  }
+
   updateURL(unitSlug) {
-    history.pushState(unitSlug, null, unitSlug);
+    const pathName = unitSlug !== 'all' ? unitSlug : this.mainPathName;
+    history.pushState(unitSlug, null, pathName);
   }
 }
 
